c959250f32c232707e539fb6ec58341e04a53a04
gokulakrishnan-tenext <gokul@tenext.in>
2026-06-17 08:04:04 +0530

    add vercel.json for client side routing

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ connectDB();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: [process.env.CLIENT_URL, "http://localhost:5173"],
     credentials: true,
   }),
 );
